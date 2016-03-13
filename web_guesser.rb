77e4aeb599155f8aa31072331f56a73fae3f03c5
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-03-13 16:17:38 -0600

    guessing number feature now actually working

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,20 +2,20 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
 
-$number = rand(4)
+@@number = rand(4)
 $color = "white"
 $guesses = 0
 
 def check_guess(guess)
-  if guess > $number
+  if guess > @@number
     high_guess(guess)
-  elsif guess < $number
+  elsif guess < @@number
     low_guess(guess)
-  elsif guess == $number
+  elsif guess == @@number
     $color = "green"
-    'Nice one! You got it!'
     $guesses = 0
-    $number =  rand(4)
+    @@number = rand(4)
+    'Nice one! You got it!'
   end
 end
 
@@ -23,7 +23,7 @@ def guess_num
   $guesses += 1
   if $guesses == 5
     $guesses = 0
-    $number = rand(4)
+    @@number = rand(4)
     "Sorry, you are out of guesses, the secret num has been reset."
   else
     "You have #{5 - $guesses} guesses left."
@@ -31,7 +31,7 @@ def guess_num
 end
 
 def high_guess(guess)
-  guess >= $number + 5 ? way_high : high
+  guess >= @@number + 5 ? way_high : high
 end
 
 def way_high
@@ -45,7 +45,7 @@ def high
 end
 
 def low_guess(guess)
-  guess <= $number - 5 ? way_low : low
+  guess <= @@number - 5 ? way_low : low
 end
 
 def way_low
@@ -62,5 +62,5 @@ get '/' do
   guess = params["guess"].to_i
   message = check_guess(guess)
   guess_message = guess_num
-  erb :index, :locals => { :number => $number, :message => message, :color => $color, :guess_message => guess_message }
+  erb :index, :locals => { :guess => guess, :number => @@number, :message => message, :color => $color, :guess_message => guess_message }
 end

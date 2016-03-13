4542be49af998bc16554d1ae16ae854654319e7b
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-03-13 15:19:03 -0600

    changing colors feature

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,6 +3,7 @@ require 'sinatra/reloader'
 require 'pry'
 
 NUMBER = rand(4)
+$color = "white"
 
 def check_guess(guess)
   if guess > NUMBER
@@ -10,21 +11,42 @@ def check_guess(guess)
   elsif guess < NUMBER
     low_guess(guess)
   elsif guess == NUMBER
+    $color = "green"
     'Nice one! You got it!'
   end
 end
 
 def high_guess(guess)
-  guess >= NUMBER + 5 ? "Way to high!" : "Too high!"
+  guess >= NUMBER + 5 ? way_high : high
+end
+
+def way_high
+  $color = "red"
+  "Way to high!"
+end
+
+def high
+  $color = "pink"
+  "Too high!"
 end
 
 def low_guess(guess)
-  guess <= NUMBER - 5 ? "Way to Low" : "Too Low!"
+  guess <= NUMBER - 5 ? way_low : low
+end
+
+def way_low
+  $color = "red"
+  "Way to low!"
+end
+
+def low
+  $color = "pink"
+  "Too low!"
 end
 
 
 get '/' do
   guess = params["guess"].to_i
   message = check_guess(guess)
-  erb :index, :locals => { :number => NUMBER, :message => message }
+  erb :index, :locals => { :number => NUMBER, :message => message, :color => $color }
 end
